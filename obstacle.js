3bc50bb54c2fd0e3fdd9f5e3e5d48f5434c74312
juno0403 <jkim17@avenues.org>
2017-01-17 13:12:21 -0500

    Update obstacle.js

--- obstacle.js
+++ obstacle.js
@@ -6,11 +6,13 @@
  *
  */
 
-var jx, jy;
+var jx, jy, jx2, jy2;
 
 function initializeObstacle() {
-  jx = 680;
+  jx = 750;
+  jx2 = 680;
   jy = 240;
+  jy2 = 240;
 }
 
 function updateObstacle() {
@@ -23,9 +25,16 @@ function updateObstacle() {
   } else {
     jx = jx - 8;
   }
+  if(jx2 <= 0) {
+    jx2 = 680;
+    jy2 = Math.floor((Math.random() * 480) + 1);
+  } else {
+    jx2 = jx2 - 10;
+  }
   //insert the code that draws the obstacle
   fill(178, 102, 255);
   ellipse(jx, jy, 24, 24);
+  ellipse(jx2, jy2, 24, 24);
   stroke(178, 102, 255);
   strokeWeight(4);
   line(jx - 12, jy, jx - 12, jy + 30);
@@ -33,6 +42,11 @@ function updateObstacle() {
   line(jx - 6, jy + 6, jx - 6, jy + 24);
   line(jx + 6, jy + 6, jx + 6, jy + 24);
   line(jx, jy + 12, jx, jy + 30);
+  line(jx2 - 12, jy2, jx2 - 12, jy2 + 30);
+  line(jx2 + 12, jy2, jx2 + 12, jy2 + 30);
+  line(jx2 - 6, jy2 + 6, jx2 - 6, jy2 + 24);
+  line(jx2 + 6, jy2 + 6, jx2 + 6, jy2 + 24);
+  line(jx2, jy2 + 12, jx2, jy2 + 30);
 }
 
 /** TESTING CODE **/
