c8c8147ba882e247b52b992e931b9972d49caf2e
dlevene18 <dlevene18@avenues.org>
2017-01-17 12:56:25 -0500

    Add files via upload

---
+++ character.js
@@ -0,0 +1,39 @@
+/*This character.js file will hold the functions 
+ * that intialize and udpate the character
+ *
+ *File description:
+ *This program creates the character that moves up and down using the up and down arrow keys.
+ *
+ */
+
+var yturtle;
+
+function initializeCharacter() {
+  yturtle = 430;
+}
+
+function updateCharacter() {
+  //insert code the make the character move when either the mouse
+  //moves or keys are pressed.
+  //println(keyCode);
+  if (keyCode == UP_ARROW){
+    yturtle = yturtle - 2;
+  }
+  else if (keyCode == DOWN_ARROW){
+    yturtle = yturtle + 2;
+  }
+
+  //insert the code that draws the character
+  noStroke();
+  fill(55, 132, 78);
+  ellipse(140, yturtle, 80, 30);
+  
+  
+  fill(72, 175, 103);
+  triangle(150, yturtle+7, 160, yturtle+9, 130, yturtle+20);
+  triangle(110, yturtle+5, 120, yturtle+7, 90, yturtle+10);
+  
+  
+  triangle(90, yturtle, 100, yturtle-2, 100, yturtle+2);
+  ellipse(180, yturtle, 10, 7);
+}
