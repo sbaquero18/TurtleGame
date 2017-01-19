b2d643057d2a5bb4efca92ea49874cfad4e47ff7
cwise17 <cwise17@avenues.org>
2017-01-19 13:35:21 -0500

    two jelly fish

--- background.js
+++ background.js
@@ -116,11 +116,20 @@ function updateBackground(){
 function collide(){
  var rect1 = {x: 100, y: yturtle-30, width: 80, height: 30}
  var rect2 = {x: jx-12, y: jy-12 , width: 24, height: 24}
+ var rect3 = {x: jx2-12, y: jy2-12 , width: 24, height: 24}
 
  if (rect1.x < rect2.x + rect2.width &&
   rect1.x + rect1.width > rect2.x &&
   rect1.y < rect2.y + rect2.height &&
   rect1.height + rect1.y > rect2.y) {
+    rect(100,100,100,100);
+    score = score - 10;
+   }
+ 
+ if (rect1.x < rect3.x + rect3.width &&
+  rect1.x + rect1.width > rect3.x &&
+  rect1.y < rect3.y + rect3.height &&
+  rect1.height + rect1.y > rect3.y) {
     rect(100,100,100,100);
     score = score - 10;
    }
