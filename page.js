489e19ac8b109712ad0b806fe86cef67d28dee75
dlevene18 <dlevene18@avenues.org>
2017-01-26 13:10:38 -0500

    Update page.js

--- page.js
+++ page.js
@@ -1,1 +1,20 @@
+/* Dylan Levene 
+1.25.17
+This program disables the use of arrow keys for scrolling on the HTML page so it does not interfere with the game. 
+I used this program from http://stackoverflow.com/questions/8916620/disable-arrow-key-scrolling-in-users-browser?scrlybrkr and I changed to something I understand more.*/
 
+window.addEventListener("keydown", function(e) {
+  if (keyCode == 32) {
+    e.preventDefault();
+  }
+    
+  if (keyCode == 38) {
+    e.preventDefault();
+  }
+  
+  if (keyCode == 40) {
+    e.preventDefault();
+  }
+  
+    
+}, false);
